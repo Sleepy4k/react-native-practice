d45db26086b7c3cef4a7e8ef4b000b15ae49a14b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-23 23:05:34 +0700

    chore: some style

--- screens/styles/RegisterStyle.js
+++ screens/styles/RegisterStyle.js
@@ -1,20 +1,23 @@
 // Import Core Libraries
 import { StyleSheet } from 'react-native';
 
+// Import Const
+import { Colors } from '../constant/Colors';
+
 // Import Helpers
 import { verticalScale, moderateScale } from '../helpers/Responsive';
 
 export const styles = StyleSheet.create({
   title: {
-    color: '#333',
     fontWeight: '500',
+    color: Colors.grey,
     fontFamily: 'Roboto-Medium',
     fontSize: moderateScale(28),
     marginBottom: verticalScale(30),
   },
   navigateLink: {
-    color: '#AD40AF',
     fontWeight: '700',
+    color: Colors.purple,
   },
   login: {
     flexDirection: 'row',
