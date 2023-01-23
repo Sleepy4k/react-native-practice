3c7a7ced75a3581ac937cad1dfef7dfbab296703
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-23 23:03:30 +0700

    feat: some constant

---
+++ screens/constant/AxiosHeader.js
@@ -0,0 +1,11 @@
+export const AxiosHeader = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+};
+
+export const axiosConfig = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json;charset=UTF-8',
+}
