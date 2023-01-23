3125f67fc8b0f1407b60c9b350ea0cd5652efea5
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-23 23:03:13 +0700

    chore: some component

--- screens/components/InputField.js
+++ screens/components/InputField.js
@@ -1,54 +1,80 @@
 // Import Core Libraries
+import { useState } from 'react';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+// Import Const
+import { Colors } from '../constant/Colors';
 
 // Import Styles
 import { styles } from '../styles/InputFieldStyle';
 
 export default function InputField({
-  icon,
-  label,
+  error = null,
+  label = '',
+  icon = null,
   editable = true,
-  inputType = 'default',
-  labelColor = '#808080',
-  defaultValue,
-  inputFunction,
+  hideInput = true,
+  defaultValue = null,
   inputMaxLength = 255,
-  fieldButtonLabel,
-  fieldButtonFunction,
+  inputType = 'default',
+  fieldButtonLabel = '',
+  labelColor = Colors.grey,
+  inputFunction = () => {},
+  fieldButtonFunction = () => {},
+  ...props
 }) {
+  const [hidePassword, setHidePassword] = useState(hideInput);
+
   return (
-    <View style={styles.inputGroup}>
-      {icon}
-      {inputType == 'password' ? (
-        <TextInput
-          editable={editable}
-          placeholder={label}
-          style={styles.input}
-          returnKeyType={'next'}
-          secureTextEntry={true}
-          keyboardType={inputType}
-          maxLength={inputMaxLength}
-          defaultValue={defaultValue}
-          onChangeText={inputFunction}
-          placeholderTextColor={labelColor}
-        />
-      ) : (
-        <TextInput
-          editable={editable}
-          placeholder={label}
-          style={styles.input}
-          returnKeyType={'next'}
-          secureTextEntry={false}
-          keyboardType={inputType}
-          maxLength={inputMaxLength}
-          defaultValue={defaultValue}
-          onChangeText={inputFunction}
-          placeholderTextColor={labelColor}
-        />
-      )}
-      <TouchableOpacity onPress={fieldButtonFunction} disabled={!editable}>
-        <Text style={styles.navigateLink}>{fieldButtonLabel}</Text>
-      </TouchableOpacity>
+    <View>
+      <View style={styles.inputGroup}>
+        {icon}
+        {inputType == 'password' ? (
+          <TextInput
+            autoCorrect={false}
+            editable={editable}
+            placeholder={label}
+            style={styles.input}
+            returnKeyType={'next'}
+            secureTextEntry={hidePassword}
+            keyboardType={inputType}
+            maxLength={inputMaxLength}
+            defaultValue={defaultValue}
+            onChangeText={inputFunction}
+            placeholderTextColor={labelColor}
+            {...props}
+          />
+        ) : (
+          <TextInput
+            autoCorrect={false}
+            editable={editable}
+            placeholder={label}
+            style={styles.input}
+            returnKeyType={'next'}
+            secureTextEntry={false}
+            keyboardType={inputType}
+            maxLength={inputMaxLength}
+            defaultValue={defaultValue}
+            onChangeText={inputFunction}
+            placeholderTextColor={labelColor}
+            {...props}
+          />
+        )}
+
+        <TouchableOpacity onPress={fieldButtonFunction} disabled={!editable}>
+          <Text style={styles.navigateLink}>{fieldButtonLabel}</Text>
+        </TouchableOpacity>
+
+        {inputType == 'password' && (
+          <MaterialCommunityIcons
+            onPress={() => setHidePassword(!hidePassword)}
+            name={hidePassword ? 'eye-outline' : 'eye-off-outline'}
+            style={styles.showPassword}
+          />
+        )}
+      </View>
+      {error && <Text style={styles.inputError}>{error}</Text>}
     </View>
   );
 }
