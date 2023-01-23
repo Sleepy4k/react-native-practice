98847b750eadf79834965d9a91ba4ccff4dcee83
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-23 23:04:44 +0700

    chore: auth stack navigation

--- screens/navigation/AuthStack.js
+++ screens/navigation/AuthStack.js
@@ -1,21 +1,62 @@
 // Import Core Libraries
+import { View } from 'react-native';
+import { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Import Pages
+import Home from '../Home';
 import Login from '../Login';
 import Register from '../Register';
-import ForgotPassword from '../ForgotPassword';
 
-const Stack = createStackNavigator();
+// Import Components
+import Loader from '../components/Loader';
 
 export default function AuthStack() {
+  const Stack = createStackNavigator();
+  const [initialRouteName, setInitialRouteName] = useState('');
+
+  const authUser = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('authUser');
+
+      if (userData) {
+        const user = JSON.parse(userData);
+
+        if (user.token) {
+          setInitialRouteName('Home');
+        } else {
+          setInitialRouteName('Login');
+        }
+      } else {
+        setInitialRouteName('Register');
+      }
+    } catch (error) {
+      setInitialRouteName('Register');
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      authUser();
+    }, 2000);
+  }, []);
+
   return (
-    <Stack.Navigator
-      initialRouteName={"Login"}
-      screenOptions={{ headerShown: false, animationEnabled: false }}>
-      <Stack.Screen name={"Login"} component={Login} />
-      <Stack.Screen name={"Register"} component={Register} />
-      <Stack.Screen name={"ForgotPassword"} component={ForgotPassword} />
-    </Stack.Navigator>
+    <>
+      {!initialRouteName ? (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Loader visible={true} text={'Memuat Aplikasi...'} />
+        </View>
+      ) : (
+        <Stack.Navigator
+          initialRouteName={initialRouteName}
+          screenOptions={{ headerShown: false, animationEnabled: false }}>
+          <Stack.Screen name={'Home'} component={Home} />
+          <Stack.Screen name={'Login'} component={Login} />
+          <Stack.Screen name={'Register'} component={Register} />
+        </Stack.Navigator>
+      )}
+    </>
   );
 }
