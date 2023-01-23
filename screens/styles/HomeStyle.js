10889024284e680da7b2cddc53434417b390925f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-23 23:05:19 +0700

    feat: some style

---
+++ screens/styles/HomeStyle.js
@@ -0,0 +1,11 @@
+// Import Core Libraries
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
