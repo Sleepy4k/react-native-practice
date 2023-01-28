607040372393790332821f72459947fb93da769c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-28 18:44:01 +0700

    feat: index component

---
+++ screens/components/index.js
@@ -0,0 +1,6 @@
+import Icon from './Icon';
+import Loader from './Loader';
+import InputField from './InputField';
+import CustomButton from './CustomButton';
+
+export { Icon, Loader, InputField, CustomButton };
