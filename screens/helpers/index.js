e21d13e4b53b7257a638e8433a92505b06b21062
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-28 18:44:48 +0700

    feat: index helper

---
+++ screens/helpers/index.js
@@ -0,0 +1,6 @@
+import { default as Wait } from './Wait';
+import { default as Dimension } from './Dimension';
+import { default as Responsive } from './Responsive';
+import { default as Notifcation } from './Notifcation';
+
+export { Wait, Dimension, Responsive, Notifcation };
